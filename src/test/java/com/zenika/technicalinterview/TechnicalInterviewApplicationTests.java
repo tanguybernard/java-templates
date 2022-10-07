4e8c7860fb5c5a96dc7197955fcde8ced3f8fb91
tanguybernard <bernardtanguy1pro@gmail.com>
2022-10-07 09:08:45 +0200

    first commit

---
+++ src/test/java/com/zenika/technicalinterview/TechnicalInterviewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zenika.technicalinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TechnicalInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
